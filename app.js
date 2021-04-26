1ee4f55b35f7f617bb82a2d50a2442f36132a193
Jim <jmerk12086@gmail.com>
2021-04-26 19:22:32 -0400

    feat(app.js) added routes

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res, next) => {
-    res.status(200).json({
-        message: 'working'
-    })
-})
+const conversationRoutes = require('./api/routes/conversations')
+const visitorRoutes = require('./api/routes/visitors')
+
+app.use('/conversations', conversationRoutes)
+app.use('/visitors', visitorRoutes)
 
 module.exports = app
