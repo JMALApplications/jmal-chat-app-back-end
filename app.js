60c2f5f6b4be74f31ecbae2ec3a8054bdbf239df
Jim <jmerk12086@gmail.com>
2021-04-27 17:41:13 -0400

    feat(app.js) added database connection

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config();
 
 const conversationRoutes = require('./api/routes/conversations')
 const visitorRoutes = require('./api/routes/visitors')
 
+mongoose.connect(process.env.ATLAS_LOGIN, {useNewUrlParser: true, useUnifiedTopology: true})
+
 app.use('/conversations', conversationRoutes)
-app.use('/visitors', visitorRoutes)
+
+/** Uncomment when visitors routes file has routes */
+// app.use('/visitors', visitorRoutes)
 
 module.exports = app
