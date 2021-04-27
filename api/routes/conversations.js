751504afa8c2fb866d92e3ea61f474c08d9bb33b
Jim <jmerk12086@gmail.com>
2021-04-27 17:56:15 -0400

    feat(conversations.js) updated routes

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -1,2 +1,28 @@
 const express = require('express')
 const router = express.Router()
+
+// return all conversations
+router.get('/', (req, res) => {
+    res.send('return all conversations')
+})
+
+router.route('/:id')
+    // get a conversation
+    .get((req, res) => {
+        res.send(req.params)
+    })
+    // create a conversation
+    .post((req, res) => {
+        res.send(req.params)
+    })
+    // update a conversation
+    .patch((req, res) => {
+        res.send(req.params)
+    })
+    //delete a conversation
+    .delete((req, res) => {
+        res.send(req.params)
+    })
+
+
+module.exports = router
