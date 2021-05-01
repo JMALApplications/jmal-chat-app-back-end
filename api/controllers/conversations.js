9d38a41b54ed3966510a1a8f660c6c772896e273
Jim <jmerk12086@gmail.com>
2021-05-01 14:09:10 -0400

    feat(controllers/conversations.js) updated conversations_create_conversation controller

--- api/controllers/conversations.js
+++ api/controllers/conversations.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { request } = require('../../app')
 const Conversation = require('../models/conversation')
 
 exports.conversations_get_all = (req, res, next) => {
@@ -6,7 +7,38 @@ exports.conversations_get_all = (req, res, next) => {
 }
 
 exports.conversations_create_conversation = (req, res, next) => {
-    res.send(req.params)
+    const conversation = new Conversation({
+        _id: mongoose.Types.ObjectId(),
+        visitor_id: mongoose.Types.ObjectId(),
+        visitor_online: req.body.visitor_online,
+        last_message: req.body.last_message,
+        last_message_time: req.body.last_message_time,
+        conversation_created_date: req.body.conversation_created_date,
+        visitor_data: {
+            name: req.body.visitor_data.name,
+            ip_address: req.body.visitor_data.ip_address,
+            os: req.body.visitor_data.os,
+            browser: req.body.visitor_data.browser,
+            phone: req.body.visitor_data.phone,
+            email: req.body.visitor_data.email,
+            address: req.body.visitor_data.address
+        },
+        conversation_data: {
+            messages: [req.body.conversation_data.messages]
+        }
+    })
+    conversation
+    .save()
+    .then(result => {
+        console.log(result)
+        res.status(201).json(result)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            error: err
+        })
+    })
 }
 
 exports.conversations_update_conversation = (req, res, next) => {
