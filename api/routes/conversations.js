467d2e1dce2de9168bc3746643717ea3b8568f62
Jim <jmerk12086@gmail.com>
2021-05-01 14:10:51 -0400

    feat(routes/conversations.js) updated create conversation route

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -6,13 +6,14 @@ const ConversationsController = require('../controllers/conversations')
 // return all conversations
 router.get('/', ConversationsController.conversations_get_all)
 
+// create a conversation
+router.post('/', ConversationsController.conversations_create_conversation)
+
 router.route('/:id')
     // get a conversation
     .get((req, res) => {
         res.send(req.params)
     })
-    // create a conversation
-    .post(ConversationsController.conversations_create_conversation)
     // update a conversation
     .patch(ConversationsController.conversations_update_conversation)
     //delete a conversation
