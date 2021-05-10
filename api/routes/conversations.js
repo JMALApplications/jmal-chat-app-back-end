7384ef8e9d97663f77f312d443d5b00dab2c1c8c
Jim Merk <jmerk12086@gmail.com>
2021-05-10 14:11:49 -0400

    refactor(routes/conversations.js) refactored /conversations operations

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -3,18 +3,18 @@ const router = express.Router()
 
 const ConversationsController = require('../controllers/conversations')
 
-// return all conversations
-router.get('/', ConversationsController.conversations_get_all)
-
-// create a conversation
-router.post('/', ConversationsController.conversations_create_conversation)
+router.route('/')
+    // get all conversations
+    .get(ConversationsController.conversations_get_all)
+    // create a conversation
+    .post(Conversations.conversations_create_conversation)
+    // delete a conversation
+    .delete(ConversationsController.conversations_delete_conversation)
 
 router.route('/:visitor_id')
     // get a conversation
     .get(ConversationsController.conversations_get_conversation)
     // update a conversation
     .patch(ConversationsController.conversations_update_conversation)
-
-router.delete('/', ConversationsController.conversations_delete_conversation)
 
 module.exports = router
