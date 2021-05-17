12fad41bf29bb5cb4c59f79b2d801798be6a0c5c
Jim Merk <jmerk12086@gmail.com>
2021-05-17 13:23:16 -0400

    feat(models/visitors.js) added Visitor model

---
+++ api/models/visitor.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const visitorSchema = new mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    visitor_id: mongoose.Schema.Types.ObjectId,
+    visitor_online: Boolean,
+    visitor_data: {
+        name: String,
+        ip_address: String,
+        os: String,
+        browser: String,
+        phone: String,
+        address: String
+    }
+})
+
+module.exports = mongoose.model('Visitor', visitorSchema)
