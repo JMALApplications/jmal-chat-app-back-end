965e49866531a2ba3476ba34aa336d23e05d819e
Jim <jmerk12086@gmail.com>
2021-05-18 19:20:56 -0400

    feat(controllers/visitors.js) added visitors_get_visitor controller

--- api/controllers/visitors.js
+++ api/controllers/visitors.js
@@ -15,3 +15,20 @@ exports.visitors_get_all = (req, res, next) => {
         })
     })
 }
+
+exports.visitors_get_visitor = (req, res, next) => {
+    const visitor = Visitor.find({
+        visitor_id: req.params.visitor_id
+    })
+    visitor
+    .then(result => {
+        console.log(result)
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        console.log(error)
+        res.status(500).json({
+            error: err
+        })
+    })
+}
