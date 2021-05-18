38ac24af6b795b892031e2f556b2cbaa2a84dad8
Jim <jmerk12086@gmail.com>
2021-05-18 19:21:47 -0400

    feat(routes/visitors.js) added get visitor route

--- api/routes/visitors.js
+++ api/routes/visitors.js
@@ -3,3 +3,6 @@ const router = express.Router()
 const VisitorsController = require('../controllers/visitors')
 
 router.route('/').get(VisitorsController.visitors_get_all)
+
+router.route('/:id')
+.get(VisitorsController.visitors_get_visitor)
