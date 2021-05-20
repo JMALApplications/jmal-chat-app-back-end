c45339050b646b9ba6430b56287f6d3cd7da16d7
Jim <jmerk12086@gmail.com>
2021-05-20 17:12:07 -0400

    feat(routes/visitors.js) added update visitor online route

--- api/routes/visitors.js
+++ api/routes/visitors.js
@@ -7,3 +7,6 @@ router.route('/').get(VisitorsController.visitors_get_all)
 router.route('/:id')
 .get(VisitorsController.visitors_get_visitor)
 .post(VisitorsController.visitors_create_visitor)
+
+router.route('/:id/VisitorOnline')
+.patch(VisitorsController.visitors_update_visitor_online)
