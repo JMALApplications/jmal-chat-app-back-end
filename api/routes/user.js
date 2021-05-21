18fd6ad778276563a99f7c80865b47d6e040a0a3
Jim <jmerk12086@gmail.com>
2021-05-21 15:53:31 -0400

    feat(routes/user.js) added user signup route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,4 +1,8 @@
 const express = require('express')
 const router = express.Router()
+const UserControllers = require('../controllers/user')
+
+router.route('/signup').post(UserControllers.user_signup)
+
 
 module.exports = router;
