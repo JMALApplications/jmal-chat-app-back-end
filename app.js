4db9abd917da8c531059a0c0a540874c26a3a509
Jim <jmerk12086@gmail.com>
2021-05-21 15:32:11 -0400

    feat(app.js) updated app.js

--- app.js
+++ app.js
@@ -10,10 +10,18 @@ app.use(express.json())
 
 const conversationRoutes = require('./api/routes/conversations')
 const visitorRoutes = require('./api/routes/visitors')
+const userRoutes = require('./api/routes/user')
 
 mongoose.connect(process.env.ATLAS_LOGIN, {useNewUrlParser: true, useUnifiedTopology: true})
 
 app.use('/conversations', conversationRoutes)
 app.use('/visitors', visitorRoutes)
+app.use('/user', userRoutes)
+
+app.use((req, res, next) => {
+    const error = new Error("Resource Not Found")
+    error.status = 404
+    next(error)
+})
 
 module.exports = app
