ce58654883fcbd8c3d305d650e61d0738c730859
Jim <jmerk12086@gmail.com>
2021-05-21 15:54:09 -0400

    feat(models/user.js) updated User model

--- api/models/user.js
+++ api/models/user.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
-const visitorSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     email: { type: String, required: true },
     password: { type: String, required: true }
 })
 
-module.exports = mongoose.model('Visitor', visitorSchema)
+module.exports = mongoose.model('User', userSchema)
