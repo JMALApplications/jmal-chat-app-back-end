d313394e3b3d0a41835918f398aae45536a37c0f
Jim <jmerk12086@gmail.com>
2021-05-21 14:33:05 -0400

    feat(models/conversation.js) updated Conversation model

--- api/models/conversation.js
+++ api/models/conversation.js
@@ -3,19 +3,9 @@ const mongoose = require("mongoose")
 const conversationSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     visitor_id: mongoose.Schema.Types.ObjectId,
-    visitor_online: Boolean,
     last_message: String,
     last_message_time: Number,
     conversation_created_date: Number,
-    visitor_data: {
-        name: String,
-        ip_address: String,
-        os: String,
-        browser: String,
-        phone: String,
-        email: String,
-        address: String
-    },
     messages: [ {
         visitor_id: String,
         message_id: String,
