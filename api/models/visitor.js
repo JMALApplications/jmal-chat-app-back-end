8c8b8a949c6c4a51f1d0dc75ab1f5f9c4952aff1
Jim <jmerk12086@gmail.com>
2021-05-21 14:36:37 -0400

    feat(models/visitor.js) updated Visitor model

--- api/models/visitor.js
+++ api/models/visitor.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const visitorSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    visitor_id: mongoose.Schema.Types.ObjectId,
-    visitor_online: Boolean,
+    visitor_id: { type: mongoose.Schema.Types.ObjectId, required: true },
+    visitor_online: { type: Boolean, required: true},
     visitor_data: {
-        name: String,
-        ip_address: String,
-        os: String,
-        browser: String,
+        name: { type: String, required: true },
+        ip_address: { type: String, required: true },
+        os: { type: String, required: true },
+        browser: { type: String, required: true },
         phone: String,
         email: String,
         address: String
