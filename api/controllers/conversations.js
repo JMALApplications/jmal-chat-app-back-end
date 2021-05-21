874802a48e502f0bf45953f00b605809889cd953
Jim <jmerk12086@gmail.com>
2021-05-21 14:33:46 -0400

    feat(controllers/conversations.js) updated conversations_create_conversation controller

--- api/controllers/conversations.js
+++ api/controllers/conversations.js
@@ -44,19 +44,9 @@ exports.conversations_create_conversation = (req, res, next) => {
     const conversation = new Conversation({
         _id: id,
         visitor_id: id,
-        visitor_online: req.body.visitor_online,
         last_message: req.body.last_message,
         last_message_time: req.body.last_message_time,
         conversation_created_date: req.body.conversation_created_date,
-        visitor_data: {
-            name: req.body.visitor_data.name,
-            ip_address: req.body.visitor_data.ip_address,
-            os: req.body.visitor_data.os,
-            browser: req.body.visitor_data.browser,
-            phone: req.body.visitor_data.phone,
-            email: req.body.visitor_data.email,
-            address: req.body.visitor_data.address
-        },
         messages: [req.body.message]
     })
     conversation
