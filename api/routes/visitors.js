114a9e8f23baa303b1346a08b060530338f634f0
Jim <jmerk12086@gmail.com>
2021-05-20 22:57:50 -0400

    feat(routes/visitors.js) updated routes

--- api/routes/visitors.js
+++ api/routes/visitors.js
@@ -2,11 +2,13 @@ const express = require('express')
 const router = express.Router()
 const VisitorsController = require('../controllers/visitors')
 
-router.route('/').get(VisitorsController.visitors_get_all)
-
-router.route('/:id')
-.get(VisitorsController.visitors_get_visitor)
+router.route('/')
+.get(VisitorsController.visitors_get_all)
 .post(VisitorsController.visitors_create_visitor)
+
+router.route('/:id').get(VisitorsController.visitors_get_visitor)
 
 router.route('/:id/VisitorOnline')
 .patch(VisitorsController.visitors_update_visitor_online)
+
+module.exports = router
