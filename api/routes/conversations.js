aecedd0cbef2c683928c178f7533664013b3cdc3
Jim <jmerk12086@gmail.com>
2021-05-24 16:44:17 -0400

    feat(routes/conversations.js) added api auth

--- api/routes/conversations.js
+++ api/routes/conversations.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const apiAuth = require('../middleware/api-auth')
 
 const ConversationsController = require('../controllers/conversations')
 
 router.route('/')
     // get all conversations
-    .get(ConversationsController.conversations_get_all)
+    .get(apiAuth, ConversationsController.conversations_get_all)
     // delete a conversation
-    .delete(ConversationsController.conversations_delete_conversation)
+    .delete(apiAuth, ConversationsController.conversations_delete_conversation)
 
 router.route('/:visitor_id')
     // create a conversation
-    .post(ConversationsController.conversations_create_conversation)
+    .post(apiAuth, ConversationsController.conversations_create_conversation)
     // get a conversation
-    .get(ConversationsController.conversations_get_conversation)
+    .get(apiAuth, ConversationsController.conversations_get_conversation)
     // update a conversation
-    .patch(ConversationsController.conversations_update_conversation)
+    .patch(apiAuth, ConversationsController.conversations_update_conversation)
 
 module.exports = router
