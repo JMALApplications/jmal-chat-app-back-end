c646a7ecc0af8c08c6c59b828d7a8d6536af22f1
Jim <jmerk12086@gmail.com>
2021-05-24 16:48:14 -0400

    feat(routes/visitors.js) added api auth

--- api/routes/visitors.js
+++ api/routes/visitors.js
@@ -1,21 +1,22 @@
 const express = require('express')
 const router = express.Router()
 const VisitorsController = require('../controllers/visitors')
+const apiAuth = require('../middleware/api-auth')
 
 router.route('/')
-.get(VisitorsController.visitors_get_all)
-.post(VisitorsController.visitors_create_visitor)
+.get(apiAuth, VisitorsController.visitors_get_all)
+.post(apiAuth, VisitorsController.visitors_create_visitor)
 
-router.get('/:visitor_id', VisitorsController.visitors_get_visitor)
+router.get('/:visitor_id', apiAuth, VisitorsController.visitors_get_visitor)
 
-router.patch('/:visitor_id/VisitorOnline', VisitorsController.visitors_update_visitor_online)
+router.patch('/:visitor_id/VisitorOnline', apiAuth, VisitorsController.visitors_update_visitor_online)
 
-router.patch('/:visitor_id/VisitorName', VisitorsController.visitors_update_visitor_name)
+router.patch('/:visitor_id/VisitorName', apiAuth, VisitorsController.visitors_update_visitor_name)
 
-router.patch('/:visitor_id/VisitorBrowser', VisitorsController.visitors_update_visitor_browser)
+router.patch('/:visitor_id/VisitorBrowser', apiAuth, VisitorsController.visitors_update_visitor_browser)
 
-router.patch('/:visitor_id/VisitorPhone', VisitorsController.visitors_update_visitor_phone)
+router.patch('/:visitor_id/VisitorPhone', apiAuth, VisitorsController.visitors_update_visitor_phone)
 
-router.patch('/:visitor_id/VisitorEmail', VisitorsController.visitors_update_visitor_email)
+router.patch('/:visitor_id/VisitorEmail', apiAuth, VisitorsController.visitors_update_visitor_email)
 
 module.exports = router
