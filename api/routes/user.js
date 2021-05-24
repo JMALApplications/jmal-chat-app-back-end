fc08a02e1775a07d04ae33d2caf411a9c74d9eec
Jim <jmerk12086@gmail.com>
2021-05-24 16:39:22 -0400

    feat(routes/user.js) added middleware

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,7 +7,7 @@ router.post('/signup', UserControllers.user_signup)
 
 router.post('/login', UserControllers.user_sign_in)
 
-router.delete('/:id', UserControllers.user_delete_user)
+router.delete('/:id', apiAuth, UserControllers.user_delete_user)
 
 
 module.exports = router;
