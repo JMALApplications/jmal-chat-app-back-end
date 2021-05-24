d4ccd812c9bbbfad282ccfcb446d66b66ba2e8fa
Jim <jmerk12086@gmail.com>
2021-05-24 16:04:14 -0400

    feat(api/routes/user.js) added user /login route

--- api/routes/user.js
+++ api/routes/user.js
@@ -4,6 +4,8 @@ const UserControllers = require('../controllers/user')
 
 router.route('/signup').post(UserControllers.user_signup)
 
+router.post('/login', UserControllers.user_sign_in)
+
 router.route('/:id').delete(UserControllers.user_delete_user)
 
 
