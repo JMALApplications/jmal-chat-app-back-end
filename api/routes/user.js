328d090af2927cb3c69012dbea0313da02f0ad9d
Jim <jmerk12086@gmail.com>
2021-05-24 16:34:50 -0400

    refactor(routes/user.js) refactored user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const UserControllers = require('../controllers/user')
+const apiAuth = require('../middleware/api-auth')
 
-router.route('/signup').post(UserControllers.user_signup)
+router.post('/signup', UserControllers.user_signup)
 
 router.post('/login', UserControllers.user_sign_in)
 
-router.route('/:id').delete(UserControllers.user_delete_user)
+router.delete('/:id', UserControllers.user_delete_user)
 
 
 module.exports = router;
